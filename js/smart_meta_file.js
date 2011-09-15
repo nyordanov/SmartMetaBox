73bb893d411180769a52580aa6ef465fae8bc766
Will Wilson <mojowill@mac.com>
2011-09-15 15:20:06 +0100

    Fixing Mistakes in Javascript

--- js/smart_meta_file.js
+++ js/smart_meta_file.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function() {
 
-jQuery('#upload_image_button').click(function() {
- formfield = jQuery('#upload_image').attr('name');
- tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
+jQuery('#upload_file_button').click(function() {
+ formfield = jQuery('#upload_file').attr('name');
+ tb_show('', 'media-upload.php?type=file&amp;TB_iframe=true');
  return false;
 });
 
 window.send_to_editor = function(html) {
- imgurl = jQuery('img',html).attr('src');
- jQuery('#upload_image').val(imgurl);
+ imgurl = jQuery(html).attr('href');
+ jQuery('#upload_file').val(imgurl);
  tb_remove();
 }
 
--- js/smart_meta_image.js
+++ js/smart_meta_image.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function() {
 
-jQuery('#upload_file_button').click(function() {
+jQuery('#upload_image_button').click(function() {
  formfield = jQuery('#upload_image').attr('name');
- tb_show('', 'media-upload.php?type=file&amp;TB_iframe=true');
+ tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
  return false;
 });
 
 window.send_to_editor = function(html) {
- imgurl = jQuery(html).attr('href');
- jQuery('#upload_file').val(imgurl);
+ imgurl = jQuery('img',html).attr('src');
+ jQuery('#upload_image').val(imgurl);
  tb_remove();
 }
 
